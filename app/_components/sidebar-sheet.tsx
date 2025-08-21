59ca7772a4b2e48b1bc93ed51812baef31fb2d3f
Mauricio Sanches <mauriciotadeusanches@gmail.com>
2025-08-21 10:30:38 -0300

    feat: adicionado google auth prisma config

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,4 +1,10 @@
-import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import {
+  CalendarIcon,
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  MenuIcon,
+} from "lucide-react";
 import { Button } from "./ui/button";
 import {
   Sheet,
@@ -11,6 +17,14 @@ import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import Image from "next/image";
 import { quickSearchOptions } from "../_constants/search";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const SidebarButton = () => {
   return (
@@ -18,14 +32,40 @@ const SidebarButton = () => {
       <SheetHeader>
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
-      <div className="py-5 border-b flex items-center border-solid gap-3">
-        <Avatar>
+      <div className="py-5 border-b flex items-center border-solid gap-3 justify-between">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger>
+            <Button size="icon">
+              <LogInIcon></LogInIcon>
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[80%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-1 font-bold">
+              <Image
+                src="/google.svg"
+                width={18}
+                height={18}
+                alt="Fazer login com o Google"
+              ></Image>
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
         <div>
           <p className="font-bold">Mauricio Sanches</p>
           <p className="text-xs">mauriciotadeusanches@gmail.com</p>
-        </div>
+        </div> */}
       </div>
       <div className="flex flex-col gap-2 p-5 border-b border-solid py-5">
         <SheetClose asChild>
