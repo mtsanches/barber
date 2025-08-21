a44431b89fecdcfbb42bbdab89cdf81ac08f1e9d
Mauricio Sanches <mauriciotadeusanches@gmail.com>
2025-08-21 08:36:57 -0300

    feat: criacao do menu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,16 +1,83 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
-import { MenuIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogOutIcon, MenuIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { quickSearchOptions } from "../_constants/search";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="p-5 flex flex-row items-center justify-between">
         <Image alt="FSW Barber" src="/logo.png" width={120} height={18} />
-        <Button size="icon" variant="outline">
-          <MenuIcon></MenuIcon>
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline">
+              <MenuIcon></MenuIcon>
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="overflow-y-auto">
+            <SheetHeader>
+              <SheetTitle className="text-left">Menu</SheetTitle>
+            </SheetHeader>
+            <div className="py-5 border-b flex items-center border-solid gap-3">
+              <Avatar>
+                <AvatarImage src="https://images.unsplash.com/photo-1728577740843-5f29c7586afe?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+              </Avatar>
+              <div>
+                <p className="font-bold">Mauricio Sanches</p>
+                <p className="text-xs">mauriciotadeusanches@gmail.com</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 p-5 border-b border-solid py-5">
+              <SheetClose asChild>
+                <Button className="justify-start gap-2" asChild>
+                  <Link href="/">
+                    <HomeIcon size={18} />
+                    Inicio
+                  </Link>
+                </Button>
+              </SheetClose>
+              <Button className="justify-start gap-2" variant="ghost">
+                <CalendarIcon size={18} />
+                Agendamentos
+              </Button>
+            </div>
+            <div className="flex flex-col gap-1 p-5 border-b border-solid py-5">
+              {quickSearchOptions.map((option) => (
+                <Button
+                  key={option.title}
+                  className="justify-start gap-2"
+                  variant="ghost"
+                >
+                  <Image
+                    alt={option.title}
+                    src={option.imageUrl}
+                    height={18}
+                    width={18}
+                  />
+                  {option.title}
+                </Button>
+              ))}
+            </div>
+
+            <div className="flex flex-col gap-2 p-5 border-b border-solid py-5">
+              <Button variant="ghost" className="justify-start gap-2">
+                <LogOutIcon size={18} />
+                Sair da Conta
+              </Button>
+            </div>
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -83,16 +83,6 @@ const Home = async () => {
           ))}
         </div>
       </div>
-
-      <footer>
-        <Card>
-          <CardContent className="px-5 py-6">
-            <p className="text-sm text-gray-400">
-              © 2025 Copyright <span className="font-bold">FSW Barber</span>
-            </p>
-          </CardContent>
-        </Card>
-      </footer>
     </div>
   );
 };
